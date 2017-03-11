a5b8d97dc8e59064f1baa8df074358b6ef004f8d
elf <elfpkck@gmail.com>
2017-03-11 06:31:57 +0200

    Translated into Russian and Ukrainian

---
+++ i18n/PolygonsParallelToLine_ru.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../polygons_parallel_to_line_dialog_base.ui" line="14"/>
+        <source>Polygons Parallel to Line</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../polygons_parallel_to_line_dialog_base.ui" line="24"/>
+        <source>Select line layer</source>
+        <translation>Выберите линейный слой</translation>
+    </message>
+    <message>
+        <location filename="../polygons_parallel_to_line_dialog_base.ui" line="38"/>
+        <source>Select polygon layer</source>
+        <translation>Выберите полигональный слой</translation>
+    </message>
+    <message>
+        <location filename="../polygons_parallel_to_line_dialog_base.ui" line="50"/>
+        <source>Rotate only selected polygons</source>
+        <translation>Вращать только выделенные полигоны</translation>
+    </message>
+    <message>
+        <location filename="../polygons_parallel_to_line_dialog_base.ui" line="59"/>
+        <source>Distance from line</source>
+        <translation>Расстояние от линии</translation>
+    </message>
+    <message>
+        <location filename="../polygons_parallel_to_line_dialog_base.ui" line="80"/>
+        <source>Angle value</source>
+        <translation>Значение угла отклонения</translation>
+    </message>
+    <message>
+        <location filename="../polygons_parallel_to_line_dialog_base.ui" line="102"/>
+        <source>Rotate by longest edge if both angles between polygon
+edges and line segment &lt;= &apos;Angle value&apos;</source>
+        <translation>Вращать по длинному ребру, если оба угла между ребрами полигона и
+отрезком линии &lt;= значения, указанного в &quot;Значение угла отклонения&quot;</translation>
+    </message>
+</context>
+</TS>
