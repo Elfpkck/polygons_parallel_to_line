0ecd6143836305a4f7249e58c0ada1715cbf0262
Andrii Liekariev <elfpkck@gmail.com>
2023-11-16 02:40:14 +0200

    remove copyrights

--- __init__.py
+++ __init__.py
@@ -1,34 +1,3 @@
-"""
-/***************************************************************************
- PolygonsParallelToLine
-                                 A QGIS plugin
- This plugin rotates polygons parallel to line
-                              -------------------
-        begin                : 2016-03-10
-        copyright            : (C) 2016-2023 by Andrii Liekariev
-        email                : elfpkck@gmail.com
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-  This script initializes the plugin, making it known to QGIS.
-
-"""
-
-__author__ = "Andrii Liekariev"
-__date__ = "2016-03-10"
-__copyright__ = "(C) 2016-2023 by Andrii Liekariev"
-
-# This will get replaced with a git SHA1 when you do a git archive
-__revision__ = "$Format:%H$"
-
-
 # noinspection PyPep8Naming
 def classFactory(iface):
     from .src.plugin import Plugin
--- src/algorithm.py
+++ src/algorithm.py
@@ -1,32 +1,4 @@
-"""
-/***************************************************************************
- PolygonsParallelToLine
-                                 A QGIS plugin
- This plugin rotates polygons parallel to line
-                              -------------------
-        begin                : 2016-03-10
-        copyright            : (C) 2016-2023 by Andrii Liekariev
-        email                : elfpkck@gmail.com
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-"""
 from __future__ import annotations
-
-__author__ = "Andrii Liekariev"
-__date__ = "2016-03-10"
-__copyright__ = "(C) 2016-2023 by Andrii Liekariev"
-
-# This will get replaced with a git SHA1 when you do a git archive
-__revision__ = "$Format:%H$"
-
 
 from typing import Any, Optional, TYPE_CHECKING
 
